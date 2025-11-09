6369667b465c27a685845485cc0ceec447d417b8
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-09 04:15:46 -0800

    feat: add very simple homepage

---
+++ front-end/components/homepage.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+export function Homepage() {
+    return (
+        <View>
+            <Text>Omnia</Text>
+            <Text>Wellness Dashboards</Text>
+            <Text>Key Stats</Text>
+        </View>
+    );
+}
