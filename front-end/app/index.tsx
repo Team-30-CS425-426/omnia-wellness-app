73b74e868234541fb65fcacc69cb037d1c6ae732
Kevin Ye <ranchonpizza24@gmail.com>
2025-11-12 21:21:16 -0800

    feat: add minimal login page

---
+++ front-end/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import { useRouter } from 'expo-router'
+
+export default function Index(){
+    const router = useRouter()
+    return (
+        <View>
+            <Button
+                title='Log In'
+                onPress={() => router.push('./(tabs)')}
+            />
+        </View>
+    )
+}
