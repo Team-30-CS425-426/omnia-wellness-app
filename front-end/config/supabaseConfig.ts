86bffebdfdf11d351fc905053fdbe23c2046203d
Kroxsed <mail.johanramirez@gmail.com>
2025-12-01 00:51:40 -0800

    Added email confirmation to user upon signup

--- front-end/config/supabaseConfig.ts
+++ front-end/config/supabaseConfig.ts
@@ -7,11 +7,16 @@ console.log("Supabase URL from Constants:", Constants.expoConfig?.extra?.supabas
 
 const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
 const supabaseKey = Constants.expoConfig?.extra?.supabaseKey;
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
 
 if (!supabaseUrl || !supabaseKey) {
   // If this throws, check your console logs above to see if it printed "undefined"
   throw new Error('Supabase URL or Key not found in environment variables.');
 }
+
+export const SUPABASE_URL = supabaseUrl;
+export const SUPABASE_ANON_KEY = supabaseKey;
+export const SUPABASE_ANON_KEY_ALT = supabaseAnonKey;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
