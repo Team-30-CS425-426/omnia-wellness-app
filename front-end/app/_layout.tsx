f2c3c1ec4f1866e1aed34b7574c4e660b5976c25
Daisy Madera Camacho <daisymaderacamacho@Daisys-MacBook-Air-2.local>
2025-12-03 13:20:50 -0800

    Add Settings tab and notification handler for daily reminders

--- front-end/app/_layout.tsx
+++ front-end/app/_layout.tsx
@@ -1,7 +1,17 @@
 import {Stack} from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { UserProvider } from '../contexts/UserContext'
+import * as Notifications from 'expo-notifications';
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+        shouldShowBanner: true,
+        shouldShowList: true,
+    }),
+});
 
 const RootLayout = () => {
 
