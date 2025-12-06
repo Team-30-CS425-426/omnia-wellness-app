454932efa36b100453731070f018ff4b4d9e250c
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-05 16:17:27 -0800

    fix: tablayout from merge

--- front-end/app/(tabs)/_layout.tsx
+++ front-end/app/(tabs)/_layout.tsx
@@ -15,23 +15,6 @@ const COLORS = {
 };
 
 const TabsLayout = () => {
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; //import bottom tab navigator
-import HomeScreen from './home'; //import the Home screen component for the main tab navigation
-import ProfileScreen from './profile'; //import the Profile screen component
-import AddMenuScreen from './add'; //import the Add Menu screen component
-import AddMenuButton from '../components/modal'; //import the custom Add Menu Button component for modal
-import { MaterialIcons } from '@expo/vector-icons';
-
-//create a navigator object, use to define screens
-const Tab = createBottomTabNavigator();
-
-const COLORS = {
-  tabIconSelected: '#003cffff',
-  tabIconDefault: '#aaaaaa',
-};
-
-const TabsLayout = () => {
   return (
     <Tab.Navigator
       initialRouteName="home"
