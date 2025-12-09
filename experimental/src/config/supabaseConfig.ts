99026b18702191a3efd3732b8d39d5117fcd509e
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-08 23:34:19 -0800

    chore: add experimental web app for testing supabase connections

---
+++ experimental/src/config/supabaseConfig.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    'https://pflqaoqpwucsewvkjajc.supabase.co',
+    'sb_publishable_NNnruGDoKGCeLjAG4pkecg_j0Q97utA'
+)
