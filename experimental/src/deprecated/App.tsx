bb25a5d283d9bb965f2a8db93ea3012277de3f6d
Kevin Ye <ranchonpizza24@gmail.com>
2025-12-09 05:08:19 -0800

    chore: miscellaneous changes

--- experimental/src/deprecated/App.tsx
+++ experimental/src/deprecated/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { supabase } from "./config/supabaseConfig"
+import { supabase } from "../config/supabaseConfig"
 
 /*
 For testing supabase connection to frontend
