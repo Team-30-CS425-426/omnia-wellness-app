705eff3dc4cc6c874c02e3e5503d0794bcf796ff
Daisy Madera <dmaderacamacho@gmail.com>
2026-02-07 16:01:00 -0800

    Remove remaining EAS projectId

--- front-end/app.config.js
+++ front-end/app.config.js
@@ -55,9 +55,6 @@ export default{
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
       supabaseAnonKey: process.env.NEXT_ANON_SUPABASE_KEY,
-      eas: {
-        projectId: "8c8c7da2-c0ef-4d51-a244-cd539683b50c"
-      }
     }
   }
 }
